687c357adcb1ff7233cf8829c83b9d5be741dc47
sigma boy <glen38400@gmail.com>
2025-12-05 18:39:57 +0100

    nothing special

--- youtube.py
+++ youtube.py
@@ -1,6 +1,6 @@
 from yt_dlp import YoutubeDL
 
-url = "https://youtu.be/lYO4tppyEE0?si=6gSBbI9OXXjkkIab"
+url = "https://youtu.be/_Xa_v7eD2Eg?si=uh-HXo5gki1kgk9v"
 
 ydl_opts = {
     "format": "mp4/best",
